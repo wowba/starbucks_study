24f3008dd253619be992ea88ebd126676d1bd20d
wowba <leeyuwk54@gmail.com>
2023-01-05 01:05:43 +0900

    공통모듈분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const searchEl = document.querySelector('.search')
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-})
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색')
-})
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '')
-  searchInputEl.value = "";
-})
-
 const badgeEl = document.querySelector('header .badges');
 window.addEventListener('scroll', _.throttle(function () {
   if (window.scrollY > 500) {
@@ -129,6 +111,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 })
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
